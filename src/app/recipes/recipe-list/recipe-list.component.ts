96b05ec3480237697980a67cd83f03e6a4539f18
Alex <alex.janssen20@gmail.com>
2019-06-28 18:06:14 +0200

    display recipe data

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes=[];
+  recipes : Recipe[]=[
+      new Recipe('A Test Recipe', 'This is simply test', 'https://get.pxhere.com/photo/dish-meal-food-vegetable-recipe-cuisine-vegetarian-food-parmigiana-1417897.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
