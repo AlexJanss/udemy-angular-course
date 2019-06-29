d74107ce17b32dd6a66bbf4ce1398e87bc0a3ade
Alex <alex.janssen20@gmail.com>
2019-06-29 11:35:04 +0200

    use ingredient model in shopping-list

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Ingredient } from '../shared/ingredient.model';
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients = [];
+  ingredients : Ingredient[] = [
+      new Ingredient('Onions', 23),
+      new Ingredient('Aspargus', 3),
+  ];
 
   constructor() { }
 
