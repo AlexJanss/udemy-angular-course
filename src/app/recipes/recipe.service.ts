6931338955fddc66f84dfeb7fe4453e7873c0be4
Alex <alex.janssen20@gmail.com>
2019-07-07 10:27:30 +0200

    display recipe details

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,11 +1,24 @@
 import {Recipe} from "./recipe.model";
 import {EventEmitter} from "@angular/core";
+import {Ingredient} from "../shared/ingredient.model";
 
 export class RecipeService{
     recipeSelected = new EventEmitter<Recipe>();
    private recipes : Recipe[]=[
-        new Recipe('A Test Recipe', 'This is simply test', 'https://get.pxhere.com/photo/dish-meal-food-vegetable-recipe-cuisine-vegetarian-food-parmigiana-1417897.jpg'),
-        new Recipe('A Test Recipe', 'This is simply test', 'https://get.pxhere.com/photo/dish-meal-food-vegetable-recipe-cuisine-vegetarian-food-parmigiana-1417897.jpg'),
+        new Recipe('A Test Recipe',
+            'This is simply test',
+            'https://get.pxhere.com/photo/dish-meal-food-vegetable-recipe-cuisine-vegetarian-food-parmigiana-1417897.jpg',
+            [
+                new Ingredient('Lettuce', 1),
+                new Ingredient('Oranges', 6),
+            ]),
+        new Recipe('A Test Recipe',
+            'This is simply test',
+            'https://get.pxhere.com/photo/dish-meal-food-vegetable-recipe-cuisine-vegetarian-food-parmigiana-1417897.jpg',
+            [
+                new Ingredient('Falafel', 12),
+                new Ingredient('Garlic', 1),
+            ]),
     ];
 
 getRecipes(){
