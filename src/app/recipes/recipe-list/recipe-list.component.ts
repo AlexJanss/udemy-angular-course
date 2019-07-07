d4be86444ea850e4ff94eb0b214b4ea8c0732bc8
Alex <alex.janssen20@gmail.com>
2019-07-07 09:28:36 +0200

    refactor code to extract recipe items from service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,26 +1,25 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
-import { Recipe } from '../recipe.model';
+import {Recipe} from '../recipe.model';
+import {RecipeService} from "../recipe.service";
 
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+    selector: 'app-recipe-list',
+    templateUrl: './recipe-list.component.html',
+    styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes : Recipe[]=[
-      new Recipe('A Test Recipe', 'This is simply test', 'https://get.pxhere.com/photo/dish-meal-food-vegetable-recipe-cuisine-vegetarian-food-parmigiana-1417897.jpg')
-  ];
-  constructor() { }
+    @Output() recipeWasSelected = new EventEmitter<Recipe>();
+    recipes: Recipe[];
 
-  ngOnInit() {
-  }
+    constructor(private recipeService: RecipeService) {
+    }
 
-  onRecipeSelected(recipe : Recipe){
+    ngOnInit() {
+        this.recipes = this.recipeService.getRecipes();
+    }
 
-    this.recipeWasSelected.emit(recipe);
-
-  }
-
+    onRecipeSelected(recipe: Recipe) {
+        this.recipeWasSelected.emit(recipe);
+   }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,3 +1,13 @@
-export class RecipeService{
+import {Recipe} from "./recipe.model";
 
+export class RecipeService{
+   private recipes : Recipe[]=[
+        new Recipe('A Test Recipe', 'This is simply test', 'https://get.pxhere.com/photo/dish-meal-food-vegetable-recipe-cuisine-vegetarian-food-parmigiana-1417897.jpg'),
+        new Recipe('A Test Recipe', 'This is simply test', 'https://get.pxhere.com/photo/dish-meal-food-vegetable-recipe-cuisine-vegetarian-food-parmigiana-1417897.jpg'),
+    ];
+
+getRecipes(){
+    // return a copy of the recipes array
+    return this.recipes.slice();
+}
 }
