456b15eebab247d27c7f10d2a660c78a64b4ae57
Alex <alex.janssen20@gmail.com>
2019-07-13 10:26:59 +0200

    load ingredient in form and display editMode

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {Ingredient} from "../../shared/ingredient.model";
 import {ShoppingListService} from "../shopping-list.service";
 import {NgForm} from "@angular/forms";
@@ -10,6 +10,7 @@ import {Subscription} from "rxjs";
     styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit, OnDestroy {
+    @ViewChild('f', { static: false }) slForm: NgForm;
     subscription: Subscription;
     editMode = false;
     editedItemIndex: number;
@@ -24,6 +25,11 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
                 (index: number) => {
                     this.editedItemIndex = index;
                     this.editMode = true;
+                    this.editedItem = this.shoppingListService.getIngredient(index);
+                    this.slForm.setValue({
+                        name: this.editedItem.name,
+                        amount: this.editedItem.amount
+                    })
                 }
             );
     }
