873bc972175e216ee919beb01db7cc5f84819c0c
Alex <alex.janssen20@gmail.com>
2019-07-19 20:37:39 +0200

    use backend data from fire base

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,24 +8,24 @@ import {Subject} from "rxjs";
 @Injectable()
 export class RecipeService{
     recipesChanged = new Subject<Recipe[]>();
-    // private recipes: Recipe[] = [];
+    private recipes: Recipe[] = [];
 
-   private recipes : Recipe[]=[
-        new Recipe('A Salad',
-            'Delicious salad',
-            'https://get.pxhere.com/photo/dish-meal-food-vegetable-recipe-cuisine-vegetarian-food-parmigiana-1417897.jpg',
-            [
-                new Ingredient('Lettuce', 1),
-                new Ingredient('Oranges', 6),
-            ]),
-        new Recipe('Falafel Recipe',
-            'Very nice falafel',
-            'https://get.pxhere.com/photo/dish-meal-food-vegetable-recipe-cuisine-vegetarian-food-parmigiana-1417897.jpg',
-            [
-                new Ingredient('Falafel', 12),
-                new Ingredient('Garlic', 1),
-            ]),
-    ];
+   // private recipes : Recipe[]=[
+   //      new Recipe('A Salad',
+   //          'Delicious salad',
+   //          'https://get.pxhere.com/photo/dish-meal-food-vegetable-recipe-cuisine-vegetarian-food-parmigiana-1417897.jpg',
+   //          [
+   //              new Ingredient('Lettuce', 1),
+   //              new Ingredient('Oranges', 6),
+   //          ]),
+   //      new Recipe('Falafel Recipe',
+   //          'Very nice falafel',
+   //          'https://get.pxhere.com/photo/dish-meal-food-vegetable-recipe-cuisine-vegetarian-food-parmigiana-1417897.jpg',
+   //          [
+   //              new Ingredient('Falafel', 12),
+   //              new Ingredient('Garlic', 1),
+   //          ]),
+   //  ];
 
     constructor(private shoppingListService: ShoppingListService) {}
 
