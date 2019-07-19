91ec30b2b25889fb50766654153232b3ca1ad8fc
Alex <alex.janssen20@gmail.com>
2019-07-19 20:20:33 +0200

    add recipe resolve service and httpmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import {ShoppingListService} from "./shopping-list/shopping-list.service";
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import {RecipeService} from "./recipes/recipe.service";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -35,6 +36,7 @@ import {RecipeService} from "./recipes/recipe.service";
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
+    HttpClientModule,
     FormsModule
   ],
   providers: [ShoppingListService, RecipeService],
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,25 +8,32 @@ import {Subject} from "rxjs";
 @Injectable()
 export class RecipeService{
     recipesChanged = new Subject<Recipe[]>();
-
-   private recipes : Recipe[]=[
-        new Recipe('A Salad',
-            'Delicious salad',
-            'https://get.pxhere.com/photo/dish-meal-food-vegetable-recipe-cuisine-vegetarian-food-parmigiana-1417897.jpg',
-            [
-                new Ingredient('Lettuce', 1),
-                new Ingredient('Oranges', 6),
-            ]),
-        new Recipe('Falafel Recipe',
-            'Very nice falafel',
-            'https://get.pxhere.com/photo/dish-meal-food-vegetable-recipe-cuisine-vegetarian-food-parmigiana-1417897.jpg',
-            [
-                new Ingredient('Falafel', 12),
-                new Ingredient('Garlic', 1),
-            ]),
-    ];
+    private recipes: Recipe[] = [];
+   //
+   // private recipes : Recipe[]=[
+   //      new Recipe('A Salad',
+   //          'Delicious salad',
+   //          'https://get.pxhere.com/photo/dish-meal-food-vegetable-recipe-cuisine-vegetarian-food-parmigiana-1417897.jpg',
+   //          [
+   //              new Ingredient('Lettuce', 1),
+   //              new Ingredient('Oranges', 6),
+   //          ]),
+   //      new Recipe('Falafel Recipe',
+   //          'Very nice falafel',
+   //          'https://get.pxhere.com/photo/dish-meal-food-vegetable-recipe-cuisine-vegetarian-food-parmigiana-1417897.jpg',
+   //          [
+   //              new Ingredient('Falafel', 12),
+   //              new Ingredient('Garlic', 1),
+   //          ]),
+   //  ];
 
     constructor(private shoppingListService: ShoppingListService) {}
+
+
+    setRecipes(recipes: Recipe[]) {
+        this.recipes = recipes;
+        this.recipesChanged.next(this.recipes.slice());
+    }
 
     getRecipes(){
         // return a copy of the recipes array
